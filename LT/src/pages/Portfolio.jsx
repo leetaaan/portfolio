92e8d56861138ed396555de428bd9f80a2326208
leetaaan <119284828+leetaaan@users.noreply.github.com>
2024-12-28 17:52:47 +0700

    second commit

--- LT/src/pages/Portfolio.jsx
+++ LT/src/pages/Portfolio.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Image from "../img/portfolio/1.png";
+import Image from "../img/avatar.png";
 import { Link } from "react-router-dom";
 
 import { motion } from "framer-motion";
